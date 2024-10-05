07855f07a9289098456dbb3a81bc80713e052ce3
itzamanjain <aman.044063@tmu.ac.in>
2024-10-05 23:53:30 +0530

    Refactor page component: Remove unused link to documentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,14 +39,6 @@ export default function Home() {
             />
             Play now
           </Link>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
