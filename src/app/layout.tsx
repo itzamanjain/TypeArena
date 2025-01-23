69f4022cd8b2830bc16c0c3f897413f85a0b97f7
itzamanjain <aman.044063@tmu.ac.in>
2025-01-23 17:00:41 +0530

    add Google Ads script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6624328231679034"
+      crossOrigin="anonymous"></script>
+        </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       > 
